36934e6b412e1fb9d31a927a4da971a8e82d6744
Andres M. Prieto <andresprieto2703@gmail.com>
2023-11-28 00:38:34 -0500

    AlexNet
    Adding Alex Net

---
+++ labs/task/laboratory_3/architectures/alex_net.py
@@ -0,0 +1,54 @@
+import torch.nn as nn
+
+# Definición del modelo
+
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(AlexNet, self).__init__()
+        self.characteristic = nn.Sequential(
+            # Bloque morado
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(96),
+            # Bloque azul 1
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=0),
+            # Bloque azul 2
+            nn.Conv2d(64, 256, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256),
+            # Bloque azul 3
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=0),
+            # Bloque azul 4
+            nn.Conv2d(256, 384, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            # Bloque azul 6
+            nn.Conv2d(384, 384, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            # Bloque azul 7
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            # Bloque azul 8
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=0),
+        )
+
+        self.flatter = nn.Sequential(
+            nn.AdaptiveAvgPool2d((6, 6)),
+            nn.Flatten()
+        )
+
+        self.classificator_nn = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(6 * 6 * 256, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes)
+        )
+
+    def forward(self, x_data):
+        value_tracker = self.characteristic(x_data)
+        value_tracker = self.flatter(value_tracker)
+        value_tracker = self.classificator_nn(value_tracker)
+
+        return value_tracker
--- labs/task/laboratory_3/architectures/vgg_16.py
+++ labs/task/laboratory_3/architectures/vgg_16.py
@@ -2,72 +2,74 @@ import torch.nn as nn
 
 # Definición del modelo
 
+
 class VGG16(nn.Module):
-  def __init__(self, num_classes = 1000):
-    super(VGG16, self).__init__()
-    self.characteristic = nn.Sequential(
-        # Bloque 1
-        nn.Conv2d(3, 64, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(64, 64, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernel_size=2, stride=2),
+    def __init__(self, num_classes=1000):
+        super(VGG16, self).__init__()
+        self.characteristic = nn.Sequential(
+            # Bloque 1
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-        # Bloque 2
-        nn.Conv2d(64, 128, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(128, 128, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernel_size=2, stride=2),
+            # Bloque 2
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-        # Bloque 3
-        nn.Conv2d(128, 256, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(256, 256, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(256, 256, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernel_size=2, stride=2),
+            # Bloque 3
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-        # Bloque 4
-        nn.Conv2d(256, 512, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(512, 512, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(512, 512, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernel_size=2, stride=2),
+            # Bloque 4
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-        # Bloque 5
-        nn.Conv2d(512, 512, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(512, 512, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(512, 512, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernel_size=2, stride=2),
-    )
+            # Bloque 5
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
 
-    # Bloque 6
-    self.flatter = nn.Sequential(
-        nn.AdaptiveAvgPool2d((7, 7)),
-        nn.Flatten()
-    )
+        # Bloque 6
+        self.flatter = nn.Sequential(
+            nn.AdaptiveAvgPool2d((7, 7)),
+            nn.Flatten()
+        )
 
-    self.classificator_nn = nn.Sequential(
-        nn.Dropout(),
-        nn.Linear(7 * 7 * 512, 4096),                       # FC - 1
-        nn.ReLU(inplace = True),
-        nn.Linear(4096, 4096),                              # FC - 2
-        nn.ReLU(inplace = True),
-        nn.Linear(4096, num_classes),                       # FC - 3
-        nn.ReLU(inplace = True),
-        nn.Softmax(dim = 1)                                 # Output
-    )
+        self.classificator_nn = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(7 * 7 * 512, 4096),                       # FC - 1
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 4096),                              # FC - 2
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),                       # FC - 3
+            nn.ReLU(inplace=True),
+            nn.Softmax(dim=1)                                 # Output
+        )
 
-  def forward(self, x_data):
-    value_tracker = self.characteristic(x_data)
-    value_tracker = self.flatter(value_tracker)
-    value_tracker = value_tracker.view(value_tracker.size(0), -1)
-    value_tracker = self.classificator_nn(value_tracker)
-    return value_tracker
+    def forward(self, x_data):
+        value_tracker = self.characteristic(x_data)
+        value_tracker = self.flatter(value_tracker)
+        value_tracker = value_tracker.view(value_tracker.size(0), -1)
+        value_tracker = self.classificator_nn(value_tracker)
+
+        return value_tracker
