6a1cab757ab1f55fe0315b720f2e6fb459dc7331
Andres M. Prieto <andresprieto2703@gmail.com>
2023-11-29 03:35:45 -0500

    Windows fix
    Fixing an small platform issue

--- labs/task/laboratory_3/util/logger.py
+++ labs/task/laboratory_3/util/logger.py
@@ -1,10 +1,14 @@
 import subprocess
+import platform
 
 
 def expected_time(msg):
     try:
-        subprocess.run(f"figlet {msg} | lolcat",
-                       shell=True, stdout=subprocess.PIPE, text=True)
+        if platform.system() == "Windows":
+            print(msg)
+        else:
+            subprocess.run(f"figlet {msg} | lolcat",
+                           shell=True, stdout=subprocess.PIPE, text=True)
     except FileNotFoundError:
         print(f"{msg}")
     except subprocess.CalledProcessError as error:
