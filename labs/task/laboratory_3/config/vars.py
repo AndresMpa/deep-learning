bf76902d3cb2d79b94472159810626eff671693a
Andres M. Prieto <andresprieto2703@gmail.com>
2023-11-29 00:13:45 -0500

    Tracking new vars
    Adding Vars into config

--- labs/task/laboratory_3/config/vars.py
+++ labs/task/laboratory_3/config/vars.py
@@ -7,20 +7,27 @@ import os
 load_dotenv()
 
 env_vars = SimpleNamespace(
-    dataset=os.environ.get("DATASET"),
+    # Net props
     net_arch=os.environ.get("NET_ARCH"),
     use_cuda=os.environ.get("USE_CUDA") == "1",
-    img_size=int(os.environ.get("IMG_SIZE")),
 
-    # Data sets directory
+    # Data
+    dataset=os.environ.get("DATASET"),
     data_path=os.environ.get("DATA_PATH"),
     batch_size=int(os.environ.get("BATCH_SIZE")),
 
-    # Results directory
-    results_data=os.environ.get("RESULTS_DATA"),
+    # Image props
+    img_size=int(os.environ.get("IMG_SIZE")),
+    img_start_index=int(os.environ.get("IMG_START_INDEX")),
 
-    # Hyper params
+    # Hyper parameters
     iterations=int(os.environ.get("ITERATIONS")),
     learning_rate=float(os.environ.get("LEARNING_RATE")),
-    momentum_value=float(os.environ.get("MOMENTUM_VALUE"))
+    momentum_value=float(os.environ.get("MOMENTUM_VALUE")),
+
+    # Loss
+    lost_criteria=os.environ.get("LOST_CRITERIA"),
+
+    # Results directory
+    results_path=os.environ.get("RESULTS_PATH"),
 )
