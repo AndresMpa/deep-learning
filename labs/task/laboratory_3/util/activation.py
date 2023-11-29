144b9294e3b3f1db0bfd8309c1e2ca62f8cdbba4
Andres M. Prieto <andresprieto2703@gmail.com>
2023-11-29 00:50:17 -0500

    Split
    Moving activation's management into a different file

--- labs/task/laboratory_3/util/activation.py
+++ labs/task/laboratory_3/util/activation.py
@@ -1,11 +1,46 @@
 from config.vars import env_vars
 
 
+def get_activation_hook(name, activations):
+    """
+    Returns a hook function that stores the activation
+    output in a dictionary
+
+    Args:
+        name (str): Name associated with the activation
+        activations (dict): Dictionary to store activation outputs
+
+    Returns:
+        hook: Hook function to be used with register_forward_hook
+    """
+    def hook(model, input, output):
+        activations[name] = output.detach()
+    return hook
+
+
+def global_activation_hook(module, input, output):
+    """
+    Global hook function that stores the activation output in a global variable
+
+    Args:
+        module: Module associated with the forward hook
+        input: Input tensor to the module
+        output: Output tensor from the module
+    """
+    global activation
+    activation = output.detach()
+
+
 def normalize_activation(activation):
     """
-    normalize_activation Normalize an activation into a numpy array
+    Normalize an activation into a numpy array
 
-    :param activation: Should be hook in a layer like activations["conv1"]
+    Args:
+        activation: Should be hook in a layer like activations["conv1"]
+
+    Returns:
+        activation_normalized: A normalized activation using a mathematical
+        criteria
     """
     # Dims to avoid excess glare
     activation = activation[env_vars.img_start_index, :, :, :].cpu().numpy()
