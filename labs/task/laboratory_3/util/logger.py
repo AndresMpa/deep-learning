45ff90e3f864788565940224a388c07a846bcefa
Andres M. Prieto <andresprieto2703@gmail.com>
2023-11-29 02:23:58 -0500

    Quick fix

--- labs/task/laboratory_3/util/logger.py
+++ labs/task/laboratory_3/util/logger.py
@@ -3,7 +3,7 @@ import subprocess
 
 def expected_time(msg):
     try:
-        subprocess.run(["figlet", msg, "lolcat"])
+        subprocess.run(["figlet", msg, "| lolcat"])
     except FileNotFoundError:
         print(f"{msg}")
     except subprocess.CalledProcessError as error:
