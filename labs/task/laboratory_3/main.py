f7c65b8135e66cb6c6cbffca011a4ed04310e9b1
Andres Prieto <andresprieto2703@gmail.com>
2023-12-05 10:43:40 -0500

    Elapsed time
    Using a better elapsed time approach

--- labs/task/laboratory_3/main.py
+++ labs/task/laboratory_3/main.py
@@ -78,18 +78,18 @@ if __name__ == '__main__':
             lost.backward()
 
             optimizator.step()
-            iteration_lost += lost
+            iteration_lost += lost.item()
 
-            if (epochs == 0 and j % 200 == 0 or j % 200 == 0 and j > 0):
+            if (epochs == 0 and j % env_vars.catch_interval == 0 or j % env_vars.catch_interval == 0 and j > 0):
                 print(f'Epoch: {epochs + 1} \t \
-                      Iteration: {j:5d} \t \
-                      Lost: {(iteration_lost / 200):.3f}')
-                error.append(iteration_lost)
+                        Iteration: {j:5d} \t \
+                        Lost: {(iteration_lost / env_vars.catch_interval):.3f}')
+                error.append(iteration_lost / env_vars.catch_interval)
                 iteration_lost = 0
 
     end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"Took: {elapsed_time / 60.0:0.2f} minutes (Processing on GPU)")
+    elapsed_time = (end_time - start_time) / 60.0
+    print(f"Took: {elapsed_time:0.2f} minutes (Processing on GPU)")
 
     # Move the model back to the CPU
     architecture.to("cpu")
@@ -109,7 +109,7 @@ if __name__ == '__main__':
     Plotting loss function
     '''
     # Move the error to the CPU before plotting
-    error_cpu = [e.cpu().item() for e in error]
+    error_cpu = [e for e in error]
     draw_error(error_cpu, timestamp)
 
     '''
