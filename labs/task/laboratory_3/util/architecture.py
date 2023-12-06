29b20b6d82ee7010016399c58e5170b9697d1a63
Andres M. Prieto <andresprieto2703@gmail.com>
2023-12-06 16:33:16 -0500

    Using pretrained models
    Adding utilities to use pretrained models

--- labs/task/laboratory_3/util/architecture.py
+++ labs/task/laboratory_3/util/architecture.py
@@ -1,8 +1,7 @@
 import torch.nn as nn
-from torch import save
-from torch import device
-import torch.optim as optim
+from torch import save, load, device
 from torch.cuda import is_available
+import torch.optim as optim
 
 import torchvision.transforms as transforms
 
@@ -13,7 +12,8 @@ from architectures.vgg_19 import VGG19
 from architectures.alex_net import AlexNet
 from architectures.resnet import ResNet, ResidualBlock
 
-from util.dirs import create_path, create_dir
+from util.dirs import get_current_path, create_path,  check_path
+from util.dirs import create_dir, list_files
 from util.logger import expected_time
 
 
@@ -54,6 +54,16 @@ def get_architecture():
         raise ValueError(f"Unsupported architecture: {arch}")
 
 
+def get_device():
+    """
+    Define the available device to run the architecture, "CPU" or "GPU"
+
+    Returns:
+        A flag as a string "CPU" or "GPU" depending on which one is available
+    """
+    return ("cuda:0" if is_available() else "cpu")
+
+
 def create_architecture():
     """
     Creates a new model using the given architecture
@@ -69,9 +79,9 @@ def create_architecture():
         # Handle the case where the architecture class is not found
         raise ValueError("Architecture class is None")
 
-    print(f'Using {"cuda: 0" if is_available() else "cpu"} for processing')
+    print(f'Using {get_device()} for processing')
 
-    arch_device = device("cuda:0" if is_available() else "cpu")
+    arch_device = device(get_device())
     architecture = net_arch
 
     architecture.to(arch_device)
@@ -150,3 +160,46 @@ def save_arch(arch, timestamp):
     print(f"Created file at: {file_path}")
 
     save(arch.state_dict(), file_path)
+
+
+def get_model_to_use():
+    """
+    Create a path for a selected (By the console) model under ".pth" format
+
+    Returns:
+        A path for a model to use
+    """
+    models_path = env_vars.models_path
+
+    if check_path(models_path):
+        available_models = list_files(models_path, ".pth")
+
+        if len(available_models) == 0:
+            file_error = f"[ERROR]: Directory {models_path} is empty"
+            raise Exception(file_error)
+        print("Available Models:")
+        for i, model in enumerate(available_models, 1):
+            print(f"{i}. {model}")
+
+        selected_model_index = int(
+            input("Select a model (enter the corresponding number): ")) - 1
+
+        selected_model_path = get_current_path(
+            f"{models_path}" +
+            f"/{available_models[selected_model_index]}")
+
+        return selected_model_path
+    else:
+        dir_error = f"[ERROR]: Directory {models_path} does not exist"
+        raise Exception(dir_error)
+
+
+def use_model():
+    """
+    Loads a model mapping it to an instance
+
+    Returns:
+        model (Object): A instance of a pre-trained model
+    """
+    model = load(get_model_to_use(), map_location=device(get_device()))
+    return model
