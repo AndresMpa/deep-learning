6e3c6e87bc0789b40f07775b05eb7d61691935c3
Andres M. Prieto <andresprieto2703@gmail.com>
2023-12-08 08:57:00 -0500

    Using full train training set

--- labs/task/laboratory_3/in_lots.py
+++ labs/task/laboratory_3/in_lots.py
@@ -19,8 +19,6 @@ def update_env_parameters(parameters):
 
 
 parameter_combinations = [
-    '''
-    Trainend
     {"NET_ARCH": "AlexNet", "ITERATIONS": "1", "LEARNING_RATE": "0.01"},
     {"NET_ARCH": "AlexNet", "ITERATIONS": "5", "LEARNING_RATE": "0.01"},
     {"NET_ARCH": "AlexNet", "ITERATIONS": "10", "LEARNING_RATE": "0.01"},
@@ -33,8 +31,6 @@ parameter_combinations = [
     {"NET_ARCH": "VGG16", "ITERATIONS": "10", "LEARNING_RATE": "0.01"},
     {"NET_ARCH": "VGG16", "ITERATIONS": "1", "LEARNING_RATE": "0.03"},
     {"NET_ARCH": "VGG16", "ITERATIONS": "5", "LEARNING_RATE": "0.03"},
-    '''
-
     {"NET_ARCH": "VGG16", "ITERATIONS": "10", "LEARNING_RATE": "0.03"},
 
     {"NET_ARCH": "VGG19", "ITERATIONS": "1", "LEARNING_RATE": "0.01"},
