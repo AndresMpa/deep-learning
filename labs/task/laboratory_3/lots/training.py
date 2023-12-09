7065175e465ebe2052dd6dec571bcb380b3cc6f0
Andres M. Prieto <andresprieto2703@gmail.com>
2023-12-09 04:33:42 -0500

    Tracking combination
    Using a different directory to handle with combination

---
+++ labs/task/laboratory_3/lots/training.py
@@ -0,0 +1,26 @@
+training_combinations = [
+    {"NET_ARCH": "AlexNet", "ITERATIONS": "1", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "AlexNet", "ITERATIONS": "5", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "AlexNet", "ITERATIONS": "10", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "AlexNet", "ITERATIONS": "1", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "AlexNet", "ITERATIONS": "5", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "AlexNet", "ITERATIONS": "10", "LEARNING_RATE": "0.03"},
+
+    {"NET_ARCH": "VGG16", "ITERATIONS": "1", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "VGG16", "ITERATIONS": "5", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "VGG16", "ITERATIONS": "10", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "VGG16", "ITERATIONS": "1", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "VGG16", "ITERATIONS": "5", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "VGG16", "ITERATIONS": "10", "LEARNING_RATE": "0.03"},
+
+    {"NET_ARCH": "VGG19", "ITERATIONS": "1", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "VGG19", "ITERATIONS": "5", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "VGG19", "ITERATIONS": "10", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "VGG19", "ITERATIONS": "1", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "VGG19", "ITERATIONS": "5", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "VGG19", "ITERATIONS": "10", "LEARNING_RATE": "0.03"},
+
+    {"NET_ARCH": "AlexNet", "ITERATIONS": "100", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "VGG16", "ITERATIONS": "100", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "VGG19", "ITERATIONS": "100", "LEARNING_RATE": "0.03"},
+]
